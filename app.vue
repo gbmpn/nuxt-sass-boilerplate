695af5792da5a90cfd1481e6f189ca2d6ec97989
GB M <gbm.workshop@gmail.com>
2022-08-18 18:15:52 +0200

    menu interaction

--- app.vue
+++ app.vue
@@ -1,10 +1,21 @@
 <template>
-  <nav>
-    <NuxtLink to="/">Home</NuxtLink>
-    <NuxtLink to="/projects">Projects</NuxtLink>
-    <NuxtLink to="/studio">Studio</NuxtLink>
-    <NuxtLink to="/contact">Contact</NuxtLink>
-  </nav>
+<div class="container">
+<header>
+    
+      <div class="logo f-left">
+        <NuxtLink to="/">
+          Logo
+        </NuxtLink>
+      </div>
+      <nav class="f-right">
+        <NuxtLink to="/">Home</NuxtLink>
+        <NuxtLink to="/projects">Projects</NuxtLink>
+        <NuxtLink to="/studio">Studio</NuxtLink>
+        <NuxtLink to="/contact">Contact</NuxtLink>
+      </nav>
+  </header>
+</div>
+  
   <div>
     <NuxtPage/>
   </div>
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="container">
         <h1>Contact</h1>
         <div class="grid">
             <div class="col-12 col-md-8">I'm a column 2/3 wide</div>
