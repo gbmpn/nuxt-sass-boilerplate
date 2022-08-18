8d1b4030d9abd78b72bf5b6de9d2eb4fe53bac5d
GB M <gbm.workshop@gmail.com>
2022-08-18 11:00:46 +0200

    base layer for dev

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <nav>
+    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink to="/projects">Projects</NuxtLink>
+    <NuxtLink to="/studio">Studio</NuxtLink>
+    <NuxtLink to="/contact">Contact</NuxtLink>
+  </nav>
+  <div>
+    <NuxtPage/>
+  </div>
+</template>
