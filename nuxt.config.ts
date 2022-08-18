9ae405bcd2eb3e8840bef72d36b6916ae202e391
GB M <gbm.workshop@gmail.com>
2022-08-18 11:21:01 +0200

    use scss instead of sass + add grid system

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,6 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     css: [
-        '~/assets/styles/_main.sass'
+        '~/assets/styles/_main.scss'
     ]
 });
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,5 +1,9 @@
 <template>
     <div>
         <h1>Contact</h1>
+        <div class="grid">
+            <div class="col-12 col-md-8">I'm a column 2/3 wide</div>
+            <div class="col-12 col-md-4">I'm a column 1/3 wide</div>
+        </div>
     </div>
 </template>
